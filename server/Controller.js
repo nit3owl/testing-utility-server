a6f9ea5c31f265ac9cc3a6b7faae536836ef54da
nit3owl <nit3owldev@gmail.com>
2017-10-13 23:37:27 -0700

    Try parroting POSTs

--- server/Controller.js
+++ server/Controller.js
@@ -2,7 +2,7 @@ let express = require('express');
 let router = express.Router();
 let bodyParser = require('body-parser');
 
-router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.json());
 
 router.get('/', (req, res) => {
     res.status(200).send('{ "status" : "UP" }');
@@ -13,6 +13,10 @@ router.get('/hello', (req, res) => {
     if ( req.query.user !== undefined )
         user = req.query.user;
     res.status(200).send(`hello, ${user}!`);
+});
+
+router.post('/parrot', (req, res) => {
+    res.status(200).send(req.body);
 });
 
 module.exports = router;
